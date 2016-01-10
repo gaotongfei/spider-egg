0f1064352021d3329897755c7e74ccfe392a53c6
Taff Gao <gaotongfei1995@gmail.com>
2016-01-10 02:26:52 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="spinner",
+    name="spider-egg",
     version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
     author="Taff Gao",
+    description="a template for python crawler with requests and beautifulsoup",
     author_email="gaotongfei1995@gmail.com",
+    url="https://github.com/gaotongfei/spider-egg",
     install_requires=[
         "args==0.1.0",
         "clint==0.5.1",
@@ -14,6 +16,6 @@ setup(
     ],
     entry_points='''
     [console_scripts]
-    spinner=spinner.cli:main
+    spider_egg=spider_egg.cli:main
     '''
 )
